e396f39ab7d2039a9c8f896e3e429ef64f75dacb
yeongwoo237 <85659895+yeongwoo237@users.noreply.github.com>
2025-02-12 17:44:58 +0900

    Update db_azure.php

--- metanet56-main/app/db_azure.php
+++ metanet56-main/app/db_azure.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "database-4.cbci4a0663pd.ap-northeast-3.rds.amazonaws.com";
-$username = "admin";
-$password = "ASDQdas123!";
+$servername = "metadbserver.mysql.database.azure.com";
+$username = "meta";
+$password = "ABcd1234!";
 $dbname = "hospital";
 $port = 3306;
 
